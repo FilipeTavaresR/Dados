cd8ebf5129619c1252ba1a2725ca4e3be2cb2cde
Filipe Tavares <filipe.roberto.ext@dasa.com.br>
2023-01-27 22:44:34 -0300

    view pager2 funcionando

--- app/src/main/java/com/HHTtecnologia/dados/ui/MainActivity.kt
+++ app/src/main/java/com/HHTtecnologia/dados/ui/MainActivity.kt
@@ -2,9 +2,9 @@ package com.HHTtecnologia.dados.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.HHTtecnologia.dados.R
 import com.HHTtecnologia.dados.databinding.ActivityMainBinding
-import com.HHTtecnologia.dados.enum.DiceType
 import com.vansuita.viewbinding.ViewBindingMethod
 import com.vansuita.viewbinding.viewBinding
 
@@ -13,13 +13,15 @@ class MainActivity : AppCompatActivity() {
 
     private val viewBinding by viewBinding<ActivityMainBinding>(ViewBindingMethod.INFLATE)
     private var index: Int = 0
-//    private lateinit var viewPager2: ViewPager2
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var rollerAdapter: RollerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
-        loadFragment(index)
-//        viewPager2 = viewBinding.viewPager
+        viewPager2 = viewBinding.viewPager
+        rollerAdapter = RollerAdapter(this)
+        viewPager2.adapter = rollerAdapter
 
         viewBinding.bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
@@ -32,26 +34,26 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        val btNext = viewBinding.btNext
-        val btPrevious = viewBinding.btPrevious
-
-        btPrevious.isEnabled = false
-
-        btNext.setOnClickListener {
-            index++
-            loadFragment(index)
-            btPrevious.isEnabled = true
-            if (DiceType.values().size - 1 == index)
-                btNext.isEnabled = false
-        }
-
-        btPrevious.setOnClickListener {
-            index--
-            loadFragment(index)
-            btNext.isEnabled = true
-            if (0 == index)
-                btPrevious.isEnabled = false
-        }
+//        val btNext = viewBinding.btNext
+//        val btPrevious = viewBinding.btPrevious
+//
+//        btPrevious.isEnabled = false
+//
+//        btNext.setOnClickListener {
+//            index++
+//            loadFragment(index)
+//            btPrevious.isEnabled = true
+//            if (DiceType.values().size - 1 == index)
+//                btNext.isEnabled = false
+//        }
+//
+//        btPrevious.setOnClickListener {
+//            index--
+//            loadFragment(index)
+//            btNext.isEnabled = true
+//            if (0 == index)
+//                btPrevious.isEnabled = false
+//        }
     }
 
 
