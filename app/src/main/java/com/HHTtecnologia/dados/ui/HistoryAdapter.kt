da5b6cbea82d67185b4b0c547d48c75f90d9750a
Filipe Tavares <filipe.roberto.ext@dasa.com.br>
2023-02-01 05:52:53 -0300

    history adapter

--- app/src/main/java/com/HHTtecnologia/dados/ui/HistoryAdapter.kt
+++ app/src/main/java/com/HHTtecnologia/dados/ui/HistoryAdapter.kt
@@ -1,24 +1,41 @@
 package com.HHTtecnologia.dados.ui
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.HHTtecnologia.dados.databinding.HistoryListBinding
+import com.HHTtecnologia.dados.entity.RollerResult
 
-class HistoryAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
-    override fun getItemCount(): Int {
-        return 1
+class HistoryAdapter : RecyclerView.Adapter<HistoryAdapter.ViewHolder>() {
+
+    private var rollerResult: List<RollerResult> = emptyList()
+
+    fun updateList(rollerResult: List<RollerResult>) {
+        this.rollerResult = rollerResult
     }
 
-    override fun createFragment(position: Int): Fragment {
-        return HistoryFragment.newInstance(position)
+    inner class ViewHolder(val binding: HistoryListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(rollerResult: RollerResult) {
+            binding.tvHistoryDice.text = rollerResult.diceType.name
+            binding.tvHistoryResult.text = rollerResult.result.toString()
+        }
     }
 
-//    override fun getItemCount(): Int {
-//        return DiceType.values().size * Int.MAX_VALUE
-//    }
-//
-//    override fun createFragment(position: Int): Fragment {
-//        return RollerFragment.newInstance(position % DiceType.values().size)
-//    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            HistoryListBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(rollerResult[position])
+    }
+
+    override fun getItemCount() = rollerResult.size
 
 }
